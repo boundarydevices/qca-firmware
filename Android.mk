71d9db9ca50d6815ad1dafa7c6da2e74e0411f58
Gary Bisson <gary.bisson@boundarydevices.com>
2017-12-14 16:15:53 +0100

    Add convenient Makefile and Android.mk
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

---
+++ Android.mk
@@ -0,0 +1,18 @@
+local_target_dir := $(TARGET_OUT)/etc/firmware/
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ath10k/QCA9377/hw1.0/board-sdio.bin
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := FIRMWARE
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := ath10k/QCA9377/hw1.0/firmware-sdio-5.bin
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(local_target_dir)
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_MODULE_CLASS := FIRMWARE
+include $(BUILD_PREBUILT)
