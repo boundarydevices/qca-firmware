7c849ee87c9b523820e25c3f6a8f3ecbd3b0fbe7
Gary Bisson <gary.bisson@boundarydevices.com>
2020-04-23 22:38:19 +0200

    Android.mk: update to build under Android 10
    - should now go under vendor partition
    - fixed the module class to match other firmwares
    - removed '/' in MODULE_NAME as not supported
    
    Signed-off-by: Gary Bisson <gary.bisson@boundarydevices.com>

--- Android.mk
+++ Android.mk
@@ -1,58 +1,58 @@
-local_target_dir := $(TARGET_OUT)/etc/firmware/
+local_target_dir := $(TARGET_OUT_VENDOR)/firmware
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := bdwlan30.bin
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
-LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := otp30.bin
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
-LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := qwlan30.bin
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
-LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := qca/tfbtfw11.tlv
+LOCAL_MODULE := tfbtfw11.tlv
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(local_target_dir)
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-LOCAL_MODULE_CLASS := FIRMWARE
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(local_target_dir)/qca
+LOCAL_SRC_FILES := qca/$(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := qca/tfbtnv11.bin
+LOCAL_MODULE := tfbtnv11.bin
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(local_target_dir)
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-LOCAL_MODULE_CLASS := FIRMWARE
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(local_target_dir)/qca
+LOCAL_SRC_FILES := qca/$(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := wlan/cfg.dat
+LOCAL_MODULE := cfg.dat
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(local_target_dir)
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-LOCAL_MODULE_CLASS := FIRMWARE
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(local_target_dir)/wlan
+LOCAL_SRC_FILES := wlan/$(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := wlan/qcom_cfg.ini
+LOCAL_MODULE := qcom_cfg.ini
 LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_PATH := $(local_target_dir)
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-LOCAL_MODULE_CLASS := FIRMWARE
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(local_target_dir)/wlan
+LOCAL_SRC_FILES := wlan/$(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
